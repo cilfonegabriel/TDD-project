026b1aa623e34356aec75188354e49bf8c81c262
gabicilfone <cilfonegabriel@gmail.com>
2023-03-23 12:46:19 -0300

    add method factorial

--- solver.rb
+++ solver.rb
@@ -1,4 +1,21 @@
 class Solver
+
+    def factorial(n)
+        if n<0
+            raise 'Number must be non-negative'
+        elsif(n==0)
+            factorial = 1
+        elsif
+            i = 1
+            factorial = 1
+            while i<=n do
+                factorial *= i
+                i = i+1
+            end
+        end
+        factorial
+    end
+
   def reverse(word)
     word.reverse
   end
