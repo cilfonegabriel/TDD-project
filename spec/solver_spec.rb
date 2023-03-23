0496566c6aa293c1ac639c18037169ec803aba78
gabicilfone <cilfonegabriel@gmail.com>
2023-03-23 12:33:00 -0300

    install bundle and rubocop

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../solver'
 
 describe Solver do
-    context 'Solver test' do
-      problem = Solver.new
+  context 'Solver test' do
+    problem = Solver.new
 
     it 'Create an instance of Solver class' do
       problem.should be_instance_of Solver
