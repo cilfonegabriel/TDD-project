5a6bfef4531df206862db7bbf8f19f89aff54f16
gabicilfone <cilfonegabriel@gmail.com>
2023-03-23 12:43:11 -0300

    add reverse method tesst and passed

--- solver.rb
+++ solver.rb
@@ -1,2 +1,5 @@
 class Solver
+  def reverse(word)
+    word.reverse
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,5 +7,9 @@ describe Solver do
     it 'Create an instance of Solver class' do
       problem.should be_instance_of Solver
     end
+
+    it 'returns the reverse string' do
+        expect(problem.reverse('hello')).to eql('olleh')
+    end
   end
 end
