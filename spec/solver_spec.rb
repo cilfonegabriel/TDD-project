32106c0e90dd73a68479845d28a21efa946a01f8
gabicilfone <cilfonegabriel@gmail.com>
2023-03-23 12:45:25 -0300

    add factorial methos for test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,15 +1,24 @@
+
 require_relative '../solver'
 
 describe Solver do
-  context 'Solver test' do
-    problem = Solver.new
-
+    context 'Solver test' do
+      problem = Solver.new
+      
     it 'Create an instance of Solver class' do
       problem.should be_instance_of Solver
     end
 
-    it 'returns the reverse string' do
-        expect(problem.reverse('hello')).to eql('olleh')
+    it 'returns the factorial of n' do
+      expect(problem.factorial(5)).to eq 120
+    end
+
+    it 'raises exception' do 
+      expect { problem.factorial(-5) }.to raise_exception(RuntimeError)
+    end
+
+    it 'equals 1' do 
+      expect(problem.factorial(0)).to eq 1
     end
   end
 end
